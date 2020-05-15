552ea722df1d177341a1bd0766cff263f8f6ee45
hem2182 <hemant_2182_fresco@yahoo.com>
2020-05-15 21:22:30 +0530

    Popup Modal code

--- app/_directives/modal.component.ts
+++ app/_directives/modal.component.ts
@@ -1,61 +1,60 @@
-﻿import { Component, ElementRef, Input, OnInit, OnDestroy } from '@angular/core';
-import * as $ from 'jquery';
+﻿import { Component, ElementRef, Input, OnInit, OnDestroy } from "@angular/core";
+import * as $ from "jquery";
 
-import { ModalService } from '../_services/index';
+import { ModalService } from "../_services/index";
 
 @Component({
-    moduleId: module.id.toString(),
-    selector: 'modal',
-    template: '<ng-content></ng-content>'
+  moduleId: module.id.toString(),
+  selector: "modal",
+  templateUrl: "./modal.component.html",
 })
-
 export class ModalComponent implements OnInit, OnDestroy {
-    @Input() id: string;
-    private element: JQuery;
+  @Input() id: string;
+  private element: JQuery;
 
-    constructor(private modalService: ModalService, private el: ElementRef) {
-        this.element = $(el.nativeElement);
+  constructor(private modalService: ModalService, private el: ElementRef) {
+    this.element = $(el.nativeElement);
+  }
+
+  ngOnInit(): void {
+    let modal = this;
+
+    // ensure id attribute exists
+    if (!this.id) {
+      console.error("modal must have an id");
+      return;
     }
 
-    ngOnInit(): void {
-        let modal = this;
+    // move element to bottom of page (just before </body>) so it can be displayed above everything else
+    this.element.appendTo("body");
 
-        // ensure id attribute exists
-        if (!this.id) {
-            console.error('modal must have an id');
-            return;
-        }
+    // close modal on background click
+    this.element.on("click", function (e: any) {
+      var target = $(e.target);
+      if (!(target.closest(".modal-body, modal").length > 0)) {
+        modal.close();
+      }
+    });
 
-        // move element to bottom of page (just before </body>) so it can be displayed above everything else
-        this.element.appendTo('body');
+    // add self (this modal instance) to the modal service so it's accessible from controllers
+    this.modalService.add(this);
+  }
 
-        // close modal on background click
-        this.element.on('click', function (e: any) {
-            var target = $(e.target);
-            if (!target.closest('.modal-body').length) {
-                modal.close();
-            }
-        });
+  // remove self from modal service when directive is destroyed
+  ngOnDestroy(): void {
+    this.modalService.remove(this.id);
+    this.element.remove();
+  }
 
-        // add self (this modal instance) to the modal service so it's accessible from controllers
-        this.modalService.add(this);
-    }
+  // open modal
+  open(): void {
+    this.element.show();
+    $("body").addClass("modal-open");
+  }
 
-    // remove self from modal service when directive is destroyed
-    ngOnDestroy(): void {
-        this.modalService.remove(this.id);
-        this.element.remove();
-    }
-
-    // open modal
-    open(): void {
-        this.element.show();
-        $('body').addClass('modal-open');
-    }
-
-    // close modal
-    close(): void {
-        this.element.hide();
-        $('body').removeClass('modal-open');
-    }
+  // close modal
+  close(): void {
+    this.element.hide();
+    $("body").removeClass("modal-open");
+  }
 }
--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,27 +1,50 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit } from "@angular/core";
 
-import { ModalService } from '../_services/index';
+import { ModalService } from "../_services/index";
 
 @Component({
-    moduleId: module.id.toString(),
-    templateUrl: 'home.component.html'
+  moduleId: module.id.toString(),
+  templateUrl: "home.component.html",
 })
-
 export class HomeComponent implements OnInit {
-    private bodyText: string;
+  private bodyText: string;
+  private PopupHeader: string;
+  private PopupBody: string;
+  private PopupType: PopupType;
 
-    constructor(private modalService: ModalService) {
-    }
+  constructor(private modalService: ModalService) {}
 
-    ngOnInit() {
-        this.bodyText = 'This text can be updated in modal 1';
-    }
+  ngOnInit() {
+    this.bodyText = "This text can be updated in modal 1";
+  }
 
-    openModal(id: string){
-        this.modalService.open(id);
-    }
+  openModal(id: string) {
+    this.modalService.open(id);
+  }
 
-    closeModal(id: string){
-        this.modalService.close(id);
-    }
+  closeModal(id: string) {
+    this.modalService.close(id);
+  }
+
+  Function1() {
+    console.log("Confirm is clicked");
+    this.closeModal("custom-modal-1");
+  }
+
+  AlertUser() {
+    this.ShowPopupFunction("Sample 1", "Sample 2", PopupType.Alert);
+  }
+
+  ShowPopupFunction(heading: string, body: string, type: PopupType) {
+    this.PopupHeader = heading;
+    this.PopupBody = body;
+    this.PopupType = type;
+    this.openModal("custom-modal-1");
+    // $("#myModal").modal({backdrop: "static"});
+  }
+}
+
+export enum PopupType {
+  Alert,
+  Confirm,
 }
